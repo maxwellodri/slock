7522da608993a407e90a4446175495dc3ccf2b71
Maxwell Odri <maxwellodri@gmail.com>
2021-02-07 15:32:38 +1000

    fixed patches

--- config.h
+++ config.h
@@ -18,7 +18,7 @@ static const char * message = "Suckless: Software that sucks less.";
 static const char * text_color = "#ffffff";
 
 /* text size (must be a valid size) */
-static const char * font_name = "6x10";
+static const char * font_name = "6x13";
 
 /*Blur*/
 static const int blurRadius=5;
